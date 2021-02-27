b3a60589402502b927d46ad479c67b976efd3345
sebknight <sebastianknightmaddox@gmail.com>
2021-02-28 00:13:14 +1300

    render projects with contentful and graphql

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken:
+    process.env.CONTENTFUL_ACCESS_TOKEN ||
+    process.env.CONTENTFUL_DELIVERY_TOKEN,
+};
+
+const { spaceId, accessToken } = contentfulConfig;
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the access token need to be provided."
+  );
+}
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: spaceId,
+        accessToken: accessToken,
+        host: 'cdn.contentful.com',
+        downloadLocal: true
+      },
+    },
+  ],
+}
--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -1,64 +1,72 @@
 import React from 'react';
-import CardHolder from '../CardHolder';
+import { graphql, useStaticQuery } from 'gatsby';
 import Subheading from '../Subheading';
 import Wrapper from '../Wrapper';
+import Paragraph from '../Paragraph';
+import styled from 'styled-components';
+import TextLink from '../TextLink';
+import { path, pipe, map, values } from 'ramda';
+import { renderRichText } from 'gatsby-source-contentful/rich-text';
+import { INLINES } from '@contentful/rich-text-types';
+
+const Title = styled.h3`
+  background: white;  
+  font-size: 1.5rem;
+  font-weight: bold;
+  border-radius: 5px;
+  font-family: 'Noto Serif', serif;
+  text-align: center;
+  padding-bottom: 2rem;
+`;
+
+const renderOptions = {
+  renderNode: {
+    [INLINES.HYPERLINK]: ({ data }, children) => (
+      <TextLink
+        href={data.uri}
+        rel={'noopener noreferrer'}
+      >{children}</TextLink>
+    )
+  },
+};
+
+
+
+
+const mapProjects = map(project => renderProject(project.node));
+
+const renderProject = (project) => 
+    <>
+      <Title>
+        {project.title}
+      </Title>
+      <Paragraph>
+        {renderRichText(project.description, renderOptions)}
+      </Paragraph>
+    </> 
+
 
 const Work = () => {
-  const height = '100%';
-  const width = '100%';
-  const gti = `${process.env.PUBLIC_URL}/assets/going-through-it.png`;
-  const runaway = `${process.env.PUBLIC_URL}/assets/runaway.png`;
-  const fetch = `${process.env.PUBLIC_URL}/assets/fetch.png`;
-
-  const cards = [
-    {
-      id: 1,
-      alt: 'Person holding an iPad with the going through it app',
-      src: gti,
-      height,
-      width,
-      content:
-      'A React SPA for users to locate nearby mental health resources utilising the Google Places API. It also features Google Charts displaying information about the use of mental health services in New Zealand. I designed and researched this project, and built the first prototype in collaboration with Tashina Jacobs and Daniel Rhodes. I recently rebuilt it using functional components.',
-      title: 'Going Through It?',
-      hrefA: 'https://going-through-it.netlify.com',
-      labelA: 'View live site',
-      hrefB: 'https://github.com/sebknight/going-through-it',
-      labelB: 'View on GitHub',
-    },
-    {
-      id: 2,
-      alt: 'A computer screen showing the WordPress theme',
-      src: runaway,
-      height,
-      width,
-      content:
-      'A clean, minimal, customisable WordPress theme originally designed for a freelance videographer. The demo site is just full of dogs. It\'s suited to anyone offering creative services -- it features a portfolio, services offered, and client testimonial sections, as well as a custom contact form so users can view enquiries directly within WordPress.',
-      title: 'Runaway WordPress Theme',
-      hrefA: 'https://sebastianmaddox.com/wordpress/',
-      labelA: 'View live site',
-      hrefB: 'https://github.com/sebknight/runaway-wordpress',
-      labelB: 'View on GitHub',
-    },
-    {
-      id: 3,
-      alt: 'IMac screen showing the Fetch app',
-      src: fetch,
-      height,
-      width,
-      content:
-      'A fun little app utilising the Fetch, Dog, and Wikipedia APIs. Click the button to get a random dog image, and you will receive a snippet of information about the breed. Done entirely in vanilla JS, and needed a surprising amount of engineering due to several edge cases.',
-      title: 'Fetch!',
-      hrefA: 'https://sebknight.github.io/fetch/',
-      labelA: 'View live site',
-      hrefB: 'https://github.com/sebknight/fetch',
-      labelB: 'View on GitHub',
-    },
-  ];
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulProject {
+        edges {
+          node {
+            title
+            description {
+              raw
+            }
+          }
+        }
+      }
+    }
+  `);
 
   return (
     <Wrapper color="#002733">
       <Subheading>Projects</Subheading>
-      <CardHolder cards={cards} />
+      {mapProjects(data.allContentfulProject.edges)}
+      {console.log(data.allContentfulProject)}
     </Wrapper>
   );
 };
